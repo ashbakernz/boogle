27a5b93349e27593c3fb4ecbd583115afdf94bd7
ashleybakernz <ashleybakernz@gmail.com>
2017-02-26 16:35:28 +1300

    Added small formatting

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php include 'partials/header.php'; ?>
-  <h1><a href="index.php"><img class="logo" src="img/logo.png"/></a></h1>
+
+  <h1>
+    <a href="index.php">
+      <img class="logo" src="img/logo.png"/>
+    </a>
+  </h1>
 
   <form method="GET" action="results.php">
     <div class="field" id="searchform">
@@ -8,4 +13,5 @@
       <button type="submit" id="search">Search</button>
     </div>
   </form>
+
 <?php include 'partials/footer.php'; ?>
