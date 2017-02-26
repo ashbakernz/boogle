9f6f950fb1204cf37cf3fad75b0e2030e58acaf7
ashleybakernz <ashleybakernz@gmail.com>
2017-02-26 15:56:23 +1300

    Added results page and styling

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <html>
   <head>
     <title>Boogle Search</title>
+   <!--  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
 
+    <link rel="stylesheet" type="text/css" href="./css/app.css">
     <link rel="stylesheet" type="text/css" href="./css/style.css">
   </head>
   <body>
@@ -12,8 +14,6 @@
           <input type="text" id="searchterm" placeholder="what do you want ?" />
 
           <button type="submit" id="search">Find</button>
-
-
 
       </div>
 
---
+++ results.php
@@ -0,0 +1,91 @@
+<html>
+  <head>
+    <title>Boogle</title>
+   <!--  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+
+    <link rel="stylesheet" type="text/css" href="./css/app.css">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-default navbar-static-top">
+      <div class="container">
+          <div class="navbar-header">
+              <!-- Collapsed Hamburger -->
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                  <span class="sr-only">Toggle Navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+
+              <!-- Branding Image -->
+              <a class="navbar-brand" href="{{ url('/') }}">
+                 <img width="80px" class="logo-top" src="img/logo.png"/>
+              </a>
+          </div>
+
+          <div class="collapse navbar-collapse" id="app-navbar-collapse">
+              <!-- Left Side Of Navbar -->
+              <ul class="nav navbar-nav">
+                <li>
+                  <a>
+                    <div class="field top-bar-search" id="searchform">
+                      <input type="text" id="searchterm" placeholder="what do you want ?" />
+
+                      <button type="submit" id="search">Find</button>
+
+                  </div>
+                  </a>
+                </li>
+              </ul>
+          </div>
+        </div>
+    </nav>
+
+    <!-- Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+          <h4>Search results for <strong>$searchQuery</strong></h4>
+
+          <!-- Displayed results -->
+          <ul class="list-group">
+          <!-- Single Result -->
+            <li class="list-group-item notification-bar-fail m-b-1">
+                <div href="#" class="notification-bar-icon">
+                  <div>
+                    <i></i>
+                  </div>
+                </div>
+                <div class="notification-bar-details">
+                  <a href="#" class="notification-bar-title">
+                  $searchResultTitle
+                  </a>
+                  <span class="text-muted">$searchResultUrl</span>
+                </div>
+            </li>
+            <!-- End of single result -->
+
+            <li class="list-group-item notification-bar-fail m-b-1">
+                <div href="#" class="notification-bar-icon">
+                  <div>
+                    <i></i>
+                  </div>
+                </div>
+                <div class="notification-bar-details">
+                  <a href="#" class="notification-bar-title">
+                  $searchResultTitle
+                  </a>
+                  <span class="text-muted">$searchResultUrl</span>
+                </div>
+            </li>
+          </ul>
+
+
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+
