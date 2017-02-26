ec33302936f0fa978229dd1c9571ea062f13b560
ashleybakernz <ashleybakernz@gmail.com>
2017-02-26 15:06:27 +1300

    We did not copy Google...
    jokes we did. hah.

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <title>Boogle Search</title>
+
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+  </head>
+  <body>
+      <h1><img class="logo" src="img/logo.png"/></h1>
+
+      <div class="field" id="searchform">
+
+          <input type="text" id="searchterm" placeholder="what do you want ?" />
+
+          <button type="submit" id="search">Find</button>
+
+
+
+      </div>
+
+  </body>
+</html>
+
