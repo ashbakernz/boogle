6edd373c60579dde3e838b1b9dd519aaf58ff6a2
mrurlwin <nz_danny_boy@hotmail.com>
2017-02-26 18:53:20 +1300

    Feedback finished

--- feedback.php
+++ feedback.php
@@ -6,28 +6,30 @@
   </a>
 </h1>
 
+<form method="POST" action="process-feedback.php">
   <div class="container">
     <h2 class="text-center">Send some feedback:</h2>
     <div class="form-group row">
       <label for="name" class="col-2 col-form-label">Your name</label>
       <div class="col-10">
-        <input class="form-control" id="name" autofocus type="text" placeholder="Jason Candle">
+        <input class="form-control" id="name" name="name" autofocus type="text" placeholder="Jason Candle">
       </div>
     </div>
     <div class="form-group row">
       <label for="email" class="col-2 col-form-label">Your Email</label>
       <div class="col-10">
-        <input class="form-control" type="email" id="email" placeholder="jason@example.com">
+        <input class="form-control" type="email" id="email" name="email" placeholder="jason@example.com">
       </div>
     </div>
     <div class="form-group row">
       <label for="feedback" class="col-2 col-form-label">Your feedback</label>
       <div class="col-10">
-        <textarea class="form-control" id="feedback" rows="3" placeholder="I like the..."></textarea>
+        <textarea class="form-control" id="feedback" name="feedback" rows="3" placeholder="I like the..."></textarea>
       </div>
     </div>
 
     <button type="submit" class="btn btn-primary pull-right">Submit</button>
   </div>
+</form>
 
 <?php include 'partials/footer.php'; ?>
--- process-feedback.php
+++ process-feedback.php
@@ -1,3 +1,15 @@
-<?php
+<?php include 'partials/header.php'; ?>
 
-die(var_dump($_POST));
+<h1>
+  <a href="index.php">
+    <img class="logo" src="img/logo.png"/>
+  </a>
+</h1>
+
+<h2 class="text-center">Thanks for the feedback <?=!empty($_POST['name'])?$_POST['name']:'';?></h2>
+<h2 class="text-center text-muted">We will reply to <?=!empty($_POST['email'])?$_POST['email']:'';?> if we need to follow up.</h2>
+<div class="text-center">
+  <a href="index.php">Home</a>
+</div>
+
+<?php include 'partials/footer.php'; ?>
