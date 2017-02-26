248f1318c4c42e8b406f373372cfa1594c7741d2
mrurlwin <nz_danny_boy@hotmail.com>
2017-02-26 20:23:50 +1300

    Validate feedback form

--- feedback.php
+++ feedback.php
@@ -10,21 +10,21 @@
   <div class="container">
     <h2 class="text-center">Send some feedback:</h2>
     <div class="form-group row">
-      <label for="name" class="col-2 col-form-label">Your name</label>
+      <label for="name" class="col-2 col-form-label">Your name <span class="text-danger">* <?=!empty($errors['name'])?$errors['name']:'';?></span></label>
       <div class="col-10">
-        <input class="form-control" id="name" name="name" autofocus type="text" placeholder="Jason Candle">
+        <input class="form-control" id="name" name="name" autofocus type="text" placeholder="Jason Candle" value="<?=!empty($name)?$name:'';?>">
       </div>
     </div>
     <div class="form-group row">
-      <label for="email" class="col-2 col-form-label">Your Email</label>
+      <label for="email" class="col-2 col-form-label">Your Email <span class="text-danger">* <?=!empty($errors['email'])?$errors['email']:'';?></span></label>
       <div class="col-10">
-        <input class="form-control" type="email" id="email" name="email" placeholder="jason@example.com">
+        <input class="form-control" type="email" id="email" name="email" placeholder="jason@example.com" value="<?=!empty($email)?$email:'';?>">
       </div>
     </div>
     <div class="form-group row">
-      <label for="feedback" class="col-2 col-form-label">Your feedback</label>
+      <label for="feedback" class="col-2 col-form-label">Your feedback <span class="text-danger">* <?=!empty($errors['feedback'])?$errors['feedback']:'';?></span></label>
       <div class="col-10">
-        <textarea class="form-control" id="feedback" name="feedback" rows="3" placeholder="I like the..."></textarea>
+        <textarea class="form-control" id="feedback" name="feedback" rows="3" placeholder="I like the..." value="<?=!empty($feedback)?$feedback:'';?>"></textarea>
       </div>
     </div>
 
--- process-feedback.php
+++ process-feedback.php
@@ -3,18 +3,43 @@
 $name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'utf-8');
 $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'utf-8');
 
-include 'partials/header.php'; ?>
+// First we must do some validation to see if we got valid data
+// define variables and set to empty values
+$name = $email = $feedback = '';
+$errors = [];
 
-<h1>
-  <a href="index.php">
-    <img class="logo" src="img/logo.png"/>
-  </a>
-</h1>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $errors['name'] = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+  if (empty($_POST["email"])) {
+    $errors['email'] = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+  if (empty($_POST["feedback"])) {
+    $errors['feedback'] = "Feedback is required";
+  } else {
+    $feedback = test_input($_POST["feedback"]);
+  }
 
-<h2 class="text-center">Thanks for the feedback <?=$name?></h2>
-<h2 class="text-center text-muted">We will reply to <?=$email?> if we need to follow up.</h2>
-<div class="text-center">
-  <a href="index.php">Home</a>
-</div>
+  if (!empty($errors)) {
+    require 'feedback.php';
+    die();
+  }
 
-<?php include 'partials/footer.php'; ?>
+  // Here is where you would send an email or save to the database etc
+  require 'thanks.php';
+  die();
+}
+
+header("Location: feedback.php");
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
